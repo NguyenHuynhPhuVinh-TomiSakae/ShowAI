b17988e1d955a3fd058768aad4237754c0bb2b3a
TomiSakae <tomisakaeanime@gmail.com>
2024-10-22 12:12:04 +0700

    fix

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,11 +1,11 @@
-import { mistral } from "@ai-sdk/mistral"
+import { google } from "@ai-sdk/google"
 import { generateText } from "ai"
 
 export async function POST(req: Request) {
     const { messages } = await req.json();
 
     const { text } = await generateText({
-        model: mistral("open-mistral-nemo"),
+        model: google("models/gemini-1.5-flash-latest") as any,
         prompt: messages[0].content
     })
 
